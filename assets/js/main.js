3376e8e623d2f4498b0bfaad2bb3a0efc8293bdb
big S studio <sanjin.spahic@daiquiri.io>
2018-10-05 01:29:06 +0200

    minor-fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,16 @@
-var modal = document.getElementById('form');
-var btn = document.getElementById("formTrigger");
-var span = document.getElementsByClassName("s-u-close")[0];
-var submit = document.getElementsByClassName("s-se-sumbit");
-
-btn.onclick = function() {
-    modal.style.display = "flex";
-}
-span.onclick = function() {
-    modal.style.display = "none";
-}
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-$(document).ready(function() {
-        $(".s-c-form").submit(function(e) {
-            e.preventDefault();
-            $(".s-c-form").hide(300);
-            $(".s-se-thanks").show(300);
-        });
+    $('.s-u-modal-wrapper').css("display", "none");
+    $('.s-se-cta').click(function(e) {
+        e.preventDefault();
+        $('.s-u-modal-wrapper').css("display", "flex");
+        $('.s-c-form').css("display", "block");
+        $('.s-se-thanks').css("display", "none");
+    });
+    $('.s-u-close').click(function (e) {
+        e.preventDefault();
+        $('.s-u-modal-wrapper').css("display", "none");
+    });
+    $('.s-se-sumbit').click(function(e) {
+        e.preventDefault();
+        $('.s-c-form').css("display", "none");
+        $('.s-se-thanks').css("display", "flex");
     });
