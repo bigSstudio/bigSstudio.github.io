69b4e5dfe8f832267da84f1b3e48bc0174d68c49
big S studio <sanjin.spahic@daiquiri.io>
2018-10-04 21:59:40 +0200

    'final-commit'

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,54 +1,23 @@
-    $(document).ready(function() {
-     $(".brackets, .cta-scroll, .cta-scroll a").click(function(t) {
-        var e = $(this).attr("href"),
-            i = $(e).offset().top;
-        $("html, body").animate({
-            scrollTop: i
-        }, 500), t.preventDefault()
-    }), $("#toggle").click(function() {
-        $("#vert-nav").toggleClass("open").show()
-    }), $(".list-inline-item a").on("click", function() {
-        $("#vert-nav").removeClass("open")
+var modal = document.getElementById('form');
+var btn = document.getElementById("formTrigger");
+var span = document.getElementsByClassName("s-u-close")[0];
+var submit = document.getElementsByClassName("s-se-sumbit");
+
+btn.onclick = function() {
+    modal.style.display = "flex";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+$(document).ready(function() {
+        $(".s-c-form").submit(function(e) {
+            e.preventDefault();
+            $(".s-c-form").hide(300);
+            $(".s-se-thanks").show(300);
+        });
     });
-    $(window).resize(function() {
-        rmvClass()
-    }), rmvClass()
-}), $(window).on("load", function() {
-    $(".loading-screen").fadeOut("slow"), AOS.init({
-        offset: 100,
-        disable: "mobile",
-        duration: 600,
-        once: !0
-    }), $(".hero-h").typeIt({
-        strings: "designing futuristic web interfaces and visual effects",
-        speed: 100,
-        html: !0,
-        lifeLike: !0,
-        autoStart: !0,
-        startDelay: 2500
-    });
-    var $container = $('.grid');
-    $container.isotope({
-        filter: '*',
-        animationOptions: {
-            duration: 750,
-            easing: 'linear',
-            queue: false
-        }
-    });
-    $('.bs-se-filter-link').click(function(){
-        $('.bs-se-filter-link .active').removeClass('active');
-        $(this).addClass('active');
- 
-        var selector = $(this).attr('data-filter');
-        $container.isotope({
-            filter: selector,
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
-            }
-         });
-         return false;
-    });
-});
